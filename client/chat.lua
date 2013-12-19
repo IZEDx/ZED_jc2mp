0b593f0939e2193eccd0e68526c9ee11a2a95fac
IZED <admin@i-zed.net>
2013-12-19 19:24:16 +0100

    Made chat size realtive to resolution

--- client/chat.lua
+++ client/chat.lua
@@ -30,10 +30,10 @@ end
 
 function ZEDChat:Render( args )
 	if(self.ChatEnabled)then
-		local y = Render.Height - 350
+		local y = Render.Height/3*1.835
 		local bigwidth = 0
 		for i,j in ipairs(self.History) do
-			if(i > #self.History - 14)then
+			if(i > #self.History - math.floor(Render.Height/4/(Render:GetTextHeight("T")+0.3)))then
 				local width = 0
 				for k,v in pairs(j) do
 					width = width + Render:GetTextWidth( v.text )
@@ -44,15 +44,15 @@ function ZEDChat:Render( args )
 			end
 		end
 		if(Chat:GetActive() and bigwidth > 0)then
-			Render:FillArea(Vector2(25,y-2), Vector2( bigwidth + 10, 225 ), Color(0,0,0,100))
+			Render:FillArea(Vector2(25,y-2), Vector2( bigwidth + 15, Render.Height/4 ), Color(0,0,0,100))
 		end
 		for i,j in ipairs(self.History) do
-			if(i > #self.History - 14)then
+			if(i > #self.History - math.floor(Render.Height/4/(Render:GetTextHeight("T")+0.3)))then
 				local x = 31
 				for k,v in pairs(j) do
-					Render:DrawText( Vector2(x,y) + Vector2( 1, 1 ), v.text, Color(0,0,0,150) )
-					Render:DrawText( Vector2(x,y) + Vector2( 2, 2 ), v.text, Color(0,0,0,50) )
-					Render:DrawText( Vector2(x,y), v.text, v.color )
+					Render:DrawText( Vector2(x,y+5) + Vector2( 1, 1 ), v.text, Color(0,0,0,150) )
+					Render:DrawText( Vector2(x,y+5) + Vector2( 2, 2 ), v.text, Color(0,0,0,50) )
+					Render:DrawText( Vector2(x,y+5), v.text, v.color )
 					x = x + Render:GetTextWidth( v.text )
 				end
 				y = y + Render:GetTextHeight("T")
